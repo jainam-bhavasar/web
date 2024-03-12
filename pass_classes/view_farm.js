1c30254d59a34b1a545291c79cf683185101876d
Jainam-IITK <jainamsb1@gmail.com>
2024-03-13 01:24:37 +0530

    Add uuid package and update uuid generation in view_farm.js

--- pass_classes/view_farm.js
+++ pass_classes/view_farm.js
@@ -1,8 +1,4 @@
-function uuidv4() {
-    return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
-        (c ^ crypto.getRandomValues(new Uint8Array(1))[0] & 15 >> c / 4).toString(16)
-    );
-}
+const { v4: uuidv4 } = require('uuid');
 
 function getViewFarmClassId(issuerId) {
     return `${issuerId}.VIEW_FARM_OFFER`;
@@ -17,7 +13,7 @@ function viewFarmOfferClass(issuerId) {
 
 function viewFarmOfferObject(issuerId, farmLink) {
     let classId = getViewFarmClassId(issuerId);
-    let uuid = uuidv4();
+    let uuid = uuidv4().toString();
     let id = `${issuerId}.${uuid}`;
     return {
         "id": id,
